987c6bb0f92a8a3bed2db7cf6439b4518a40408d
Khoa Tran <autumns.sun@gmail.com>
2013-08-19 17:25:40 +1000

    Caching, and decompose paths

--- functions.php
+++ functions.php
@@ -10,7 +10,9 @@
 	    printf("Connect failed: %s\n", mysqli_connect_error());
 	    exit();
 	}
-
+	//This function will request the translink data base directly
+	//and record the respond into the cache database with a timestamp
+	//it will return a associated array of rendeed object
 	function request_API($path){
 	    // Create connection
 	    global $host, $username, $db, $password;
@@ -92,4 +94,41 @@
 		};
 	};
 
+
+	// Caching logic // 
+	// caching will take a path and return a Assosiative array
+	// Select the path from cache database,
+	// if data stored in the cache is new, then get information from database.
+	// if data is expired, then querry OPIA and update the cache.
+	function cache_logic($rest_path) { //may be change the function name to make more sense? 
+	    $path_array = decompose_rest_path($rest_path);
+	    $model = $path_array[0];
+	    $control = $path_array[2];
+	    switch ($model) {
+	        case "version":
+	            // get direcly from the cache: 
+	            printf("model=version");
+	            break;
+	        case "location":// location contain s
+	            //Check the location cache database 
+	            printf('model=location');
+	            break;
+	        case "travel":
+	            //querry the OPIA api dirrectly do not need to check the cache database
+	            printf('model=travel');
+	            break;
+	        case "network":
+	            //querry the OPIA api dirrectly do not need to check the cache database
+	            printf('model=network');
+	            break;
+	        default :
+	            // return error: No matches service, please check your path.
+	            break;
+	    }
+	}
+	
+	
+	function decompose_rest_path($rest_path) {
+	   return list($model, $control, $view) = explode("/", $rest_path);
+	}
 ?>
