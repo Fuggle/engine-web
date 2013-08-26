c5a89cac2e39591011ca8430fe9f476f025c3893
Khoa Tran <autumns.sun@gmail.com>
2013-08-27 09:20:10 +1000

    fixing error of using bad request
    dev.php is a sand box file,

--- stops-from-location/dev.php
+++ stops-from-location/dev.php
@@ -26,8 +26,17 @@
 	$location_string=str_replace(" ","+",$location_string);
 	//die();
 	$result=querry_opia(("location/rest/resolve?input=".$location_string."&filter=".$filter."&maxResults=".$maxResult));
-	echo $result;
-	// resolve location using translink opia
+	//echo $result;
+	// resolve location using translink opia\
+	$result_array=json_decode($result,true);
 	
-	
+	while (list($key, $value) = each($result_array)) {
+    	echo "\tKey: $key; Value: $value<br />\n";
+    	while (list($key1, $value1) = each($value)) {
+    		echo( "\t\tKey: $key1; Value: $value1<br />\n");
+    		while (list($key2, $value2) = each($value1)) {
+    			echo("\t\t\tKey: $key2; Value: $value2<br />\n");
+    		}
+    	}
+    }
 ?>
