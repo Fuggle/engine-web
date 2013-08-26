dc6a2f9d58a9a3b2a608ff736d79cad6331ecdf1
Khoa Tran <autumns.sun@gmail.com>
2013-08-26 17:34:58 +1000

    stops-nearly

---
+++ cache/location/rest/stops-nearby/index.php
@@ -0,0 +1,16 @@
+<?php
+
+	// this page take in the location Id  parameter radiusM number useWalkingDistance and number of results
+	// and return near by stop NearbyStops
+	include('../../../cache_functions.php');
+	$radiusM=$_GET['radiusM'];
+	$useWalkingDistance=$_GET['useWalkingDistance'];
+	$maxResults=$_GET['maxResults'];
+	
+	
+	//LM%3AParks%20%26%20Reserves%3ASt%20Lucia%20Playground?radiusM=200&useWalkingDistance=true&maxResults=50
+	
+	
+	cache_engine("location/rest/stops-nearby/".$id);
+	
+?>
