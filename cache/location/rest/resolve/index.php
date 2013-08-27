5c2717a252c87e0a8dc6279547af738b84457358
Fuggle <whoiscatnow@gmail.com>
2013-08-27 18:01:52 +1000

    search moved into index for resolve.

--- cache/location/rest/resolve/index.php
+++ cache/location/rest/resolve/index.php
@@ -1,17 +1,36 @@
 <?php 
-//location/rest/resolve
-	include('../../../cache_functions.php');
-	$resolve_input=$_GET['input'];
-	if( isset($_GET['filter'],$_GET['maxResults'])){
-		$filter=$_GET['filter'];
-		$maxResults=$_GET['maxResults'];
-	}else{
-		$filter='0';
-		$maxResults='10';
-		
-	}
-	//echo $resolve_input;
-	querry_opia(("location/rest/resolve?input=".$resolve_input."&filter=".$filter."&maxResults=".$maxResults));
+
+    $query=$_GET['query'];
+    search("location/rest/resolve?input=".urlencode($query)."&filter=0&maxResults=25&api_key=special-key");
+
+    /*
+	This handles searching for locations, stops etc.
+	Takes a path + search query and returns an array of 
+	search results or null if there is none.
+    */
+    function search($path_querry){ 
+ 
+	    $opia_username="tran.khoa";
+		$opia_password="wNT}MGc@y+k0";
+	    $url=("https://opia.api.translink.com.au/v1/".$path_querry);
+	    
+	    $headers = array('Accept: application/json','Content-Type: application/json');
+	    
+	    //intiitial ther cURL 
+	    $curl = curl_init();
+	    
+	    curl_setopt($curl, CURLOPT_URL, $url);
+	    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	    curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
+	    curl_setopt($curl, CURLOPT_USERPWD,$opia_username.':'.$opia_password);
+	    $resp = curl_exec($curl);
 	
+	    if($resp == null) {
+	    	return null;
+	    } else {
+	    	print_r($resp);
+	    	//return $resp;
+	    }
+    }
 	
 ?>
